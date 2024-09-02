1451b5f1fa628063455b1ea3cf285cabfc205821
IT24101256 <IT24101256@my.sliit.lk>
2024-09-02 21:08:40 +0530

    Lab6Q3.java

---
+++ Q3/IT24101256Lab6Q3.java
@@ -0,0 +1,63 @@
+  import java.util.Scanner;
+  
+		public class IT24101256Lab6Q3    {
+  
+public static void main(String[] args)		{
+
+//Defining the variables
+	int num,count=0;
+	double sumOfSquares=0,rms,squareOfNum;
+	
+	
+//Creating a new Scanner class
+	Scanner input=new Scanner(System.in);
+	
+//Telling user to input the numbers to find rms
+	System.out.println("Enter positive integers (terminate input with -99):");
+	
+//Entering in to while loop to get inputs
+	while(true)
+	{
+		System.out.print("Enter a number : ");
+		num=input.nextInt();
+		
+//Terminate when -99 is entered
+		if(num==-99)
+		{
+			
+		break;
+					
+					}
+					
+//Check for negative inputs
+		if(num<0)
+		{
+			
+			System.out.println("Invalid input. Please enter a positive integer or -99 to terminate");
+			continue;
+		     }
+			 
+		 // Calculate the square of the number and add it to sumOfSquares
+            sumOfSquares += num * num;
+            count++;	 
+			 
+			}
+		
+//Creating a blank space
+	System.out.println("");
+		
+//Calculate the root mean square
+	if(count>0)
+	{
+		rms=Math.sqrt(sumOfSquares/count);
+		System.out.println("The Root Mean Square(RMS) is :" +rms);
+		
+		
+			}
+			
+	
+		
+	
+		}
+		
+}
